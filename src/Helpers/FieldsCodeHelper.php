f408ad690ae344fdbf8ddc94792afc8ed8dbe6f8
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2022-12-23 15:23:40 +0100

    Related keys merge support

--- src/Helpers/FieldsCodeHelper.php
+++ src/Helpers/FieldsCodeHelper.php
@@ -19,6 +19,7 @@ use Lkt\Factory\Schemas\Fields\JSONField;
 use Lkt\Factory\Schemas\Fields\PivotField;
 use Lkt\Factory\Schemas\Fields\RelatedField;
 use Lkt\Factory\Schemas\Fields\RelatedKeysField;
+use Lkt\Factory\Schemas\Fields\RelatedKeysMergeField;
 use Lkt\Factory\Schemas\Fields\StringChoiceField;
 use Lkt\Factory\Schemas\Fields\StringField;
 use Lkt\Factory\Schemas\Fields\UnixTimeStampField;
@@ -205,6 +206,13 @@ class FieldsCodeHelper
             if ($field instanceof JSONField) {
                 $templateData['isAssoc'] = $field->isAssoc();
                 $methods[] = Template::file(__DIR__ . '/../../assets/phtml/fields/json-field.phtml')
+                    ->setData($templateData)
+                    ->parse();
+                continue;
+            }
+
+            if ($field instanceof RelatedKeysMergeField) {
+                $methods[] = Template::file(__DIR__ . '/../../assets/phtml/fields/related-keys-merge-field.phtml')
                     ->setData($templateData)
                     ->parse();
                 continue;
